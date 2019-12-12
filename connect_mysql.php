880a0d4ae8237f245e0ce8c87b835b11fd90ab7e
HelloNewYear <36616159+HelloNewYear@users.noreply.github.com>
2019-12-12 20:56:06 +0800

    Update connect_mysql.php

--- connect_mysql.php
+++ connect_mysql.php
@@ -11,7 +11,7 @@ function insert_into_blackwidow($ip, $server){
     mysqli_select_db($con, "blackwidow") or die("Can't use blackwidow : " . mysqli_error($con));
     echo("connect blackwidow success!<br>");
 
-    $table_name = "login_log";
+    $table_name = "access_log";
     $result = mysqli_query($con, "SHOW TABLES LIKE '" . $table_name . "';");
     if(mysqli_num_rows($result)==0){
         $create_table_sql = "CREATE TABLE " . $table_name . "
