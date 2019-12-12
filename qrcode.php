cb61e88956aef56f63dd6ee7c1c6400ac6d3ac65
HelloNewYear <36616159+HelloNewYear@users.noreply.github.com>
2019-12-12 19:49:25 +0800

    Update qrcode.php

--- qrcode.php
+++ qrcode.php
@@ -1,22 +1,16 @@
 <html>
 <head>
-<link rel="stylesheet" href="https://www.bootcss.com/p/buttons/css/buttons.css" type="text/css" />
+    <title>qrcode</title>
 </head>
 <body>
 <?php
     $data = "01,10,042001800311,92220891,257.52,20191005,52791651903408788835,4C2F";
-    function generateQRfromqrserver($data,$widhtHeight ='150')
+    function generateQRfromqrserver($data, $widhtHeight ='150')
     {
     	$chl = urlencode($chl);
     	echo '<img src="https://api.qrserver.com/v1/create-qr-code/?data='.$data.'"/>';
     }
     generateQRfromqrserver($data);
-    
-    function a()
-    {
-        echo '<button></button>';
-    }
-    echo '<button type="button" class="button button-glow button-rounded button-royal" onclick=alert("abc")></button>';
 ?>
 </body>
 </html>
