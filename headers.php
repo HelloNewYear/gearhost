a8d3e7ff15d7b2a53764d3f64ee79a89deb33c30
HelloNewYear <36616159+HelloNewYear@users.noreply.github.com>
2019-12-28 22:07:23 +0800

    Update headers.php

--- headers.php
+++ headers.php
@@ -1,6 +1,11 @@
 <?php
     $headers = apache_request_headers();
+    ksort($headers);
+    foreach($headers as $header => $value){
+        echo "$header: $value <br/>";
+    }
 
-    foreach ($headers as $header => $value) {
-        echo "$header: $value <br />\n";
+    ksort($_SERVER);
+    foreach($_SERVER as $key => $value){
+        echo "$key => $value <br/>";
     }
