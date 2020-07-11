6ee7e9330ebab9c9691ecd35ea5f782621545b94
HelloNewYear <36616159+HelloNewYear@users.noreply.github.com>
2020-07-11 12:23:23 +0800

    Update CCB_Gold.php

--- CCB_Gold.php
+++ CCB_Gold.php
@@ -1,10 +1,10 @@
 <?php
     $url = "http://tool.ccb.com/webtran/static/trendchart/getAccountData.gsp?dateType=timeSharing&sec_code=019999";
-    $UA = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36";
-    $AC = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
+    $UA = "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36";
+    $AC = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
     $opt = array("http"=>array(
         "method"=>"GET",
-        "header"=>array("User-Agent:".$UA, "Accept:".$AC)
+        "header"=>array($UA, $AC)
         )
     );
     $context = stream_context_create($opt);
