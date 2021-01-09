ec284b2c9d22202b6a3795857cd3fed7b3078ef3
HelloNewYear <36616159+HelloNewYear@users.noreply.github.com>
2021-01-09 15:43:55 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
     //connect server
-    $con = mysqli_connect("den1.mysql2.gear.host", "blackwidow", base64_decode("bmF0YXNoYS4="));
+    $con = mysqli_connect("den1.mysql3.gear.host", "blackwidow", base64_decode("bmF0YXNoYS4="));
     if(!$con){die("Couldn't connect mySQL : " . mysqli_error($con));}
 
     //connect database
