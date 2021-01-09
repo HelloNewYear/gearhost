f5a45e5e6577b704c56a4b99ccabeb62086614c3
HelloNewYear <36616159+HelloNewYear@users.noreply.github.com>
2021-01-09 16:24:52 +0800

    Update database.php

--- database.php
+++ database.php
@@ -2,14 +2,11 @@
     //connect server
     $con = mysqli_connect("den1.mysql3.gear.host", "blackwidow", base64_decode("bmF0YXNoYS4="));
     if(!$con){die("Couldn't connect mySQL : " . mysqli_error($con));}
-    echo "ok2";
 
     //connect database
     $database_name = "blackwidow";
-    //mysqli_select_db($con, $database_name;) or die("Can't use blackwidow : " . mysqli_error($con));
-    echo "ok3";
+    mysqli_select_db($con, $database_name) or die("Can't use blackwidow : " . mysqli_error($con));
 
-/*
     $table_name = "access_log";
     $result = mysqli_query($con, "SHOW TABLES LIKE '" . $table_name . "';");
     if(mysqli_num_rows($result)==0){
@@ -19,16 +16,15 @@
             update_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
             server TEXT
         );";
-        echo $create_table_sql."<br>";
+        #echo $create_table_sql."<br>";
         mysqli_query($con, $create_table_sql);
         unset($create_table_sql);
     }
     unset($result);
 
     $insert_table_sql = "INSERT INTO " . $table_name . "(server) VALUES('" . json_encode($_SERVER) . "');";
-    echo $insert_table_sql."<br>";
+    #echo $insert_table_sql."<br>";
     mysqli_query($con, $insert_table_sql) or die("Error : " . mysqli_error($con));
 
     unset($insert_table_sql, $table_name);
     mysqli_close($con);
-*/
