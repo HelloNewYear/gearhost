e00c483fb0e54b1abb068527c2c8d2802b67fa96
HelloNewYear <36616159+HelloNewYear@users.noreply.github.com>
2022-12-20 19:47:22 +0800

    Update cron.php

--- cron.php
+++ cron.php
@@ -1,7 +1,7 @@
 <?php
     ignore_user_abort();  //关掉测览器,PHP脚本也可以继续执行
     set_time_limit(0);    //通过set_time_limit(0)可以让程序无限制的执行下去
-    $interval=2;
+    $interval=60*30;      //每隔半小时运行
     do{
         $run = include('cron_switch.php');
         if(!$run)die('process abort');
